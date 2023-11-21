6963596fb088775024dc15f75832465afdb77ae6
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-21 11:15:18 +0000

    add sortby

--- src/app/animals/page.tsx
+++ src/app/animals/page.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import React from "react";
 
+type petSearchQuery = {
+  sortBy: string;
+};
+
 type petsType = {
   name: string;
   id: number;
@@ -33,10 +37,47 @@ let pets: petsType[] = [
   },
 ];
 
-export default function Page() {
+//next js 'exposes' the search Params to us (search Params being the ?query=something in URLs). We're deconstructing that and then typing it.
+export default function Page({
+  searchParams,
+}: {
+  searchParams: petSearchQuery;
+}) {
+  // this compares each object in our pets array. If object a < object b, it will swap them in the array.
+  function comparePets(a: petsType, b: petsType) {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  // making a copy of the pets array so we don't mutate it. (if we just use the pets, it makes it impossible to 'undo' the sorting)
+  let sortedPets = [...pets];
+
+  if (searchParams.sortBy == "asc") {
+    pets.sort(comparePets);
+  } else if (searchParams.sortBy == "desc") {
+    pets.sort(comparePets).reverse();
+  }
+
   return (
     <div>
       <h4>This is a website about animals: here are many animals!</h4>
+      <p>{searchParams.sortBy}</p>
+      <br />
+      <Link href="/animals">Remove the sort</Link>
+      <br />
+      <br />
+      <Link href="/animals?sortBy=asc">Sort by ascending</Link>
+      <br />
+      <br />
+      <Link href="/animals?sortBy=desc">Sort by descending</Link>
+      <br />
+      <br />
+      <br />
       {pets.map((pet) => {
         return (
           <div key={pet.id}>
