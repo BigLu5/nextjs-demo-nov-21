828b64c3100d7966696bc5e183d23e286b8641c4
LUKE REYNOLDS <luke.reynolds.92@gmail.com>
2023-11-21 14:48:53 +0000

    add links to animal pages

--- src/app/animals/[pets]/page.tsx
+++ src/app/animals/[pets]/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 type petParam = { params: { pets: string } };
 
 export default function Page({ params }: petParam) {
-  return <div>This is a dynamic route parameter: {params.pets}</div>;
+  return (
+    <div>
+      <h2>This is a dynamic route parameter: {params.pets}</h2>
+      <Link href="/animals">{`<-- Go back`}</Link>
+    </div>
+  );
 }
